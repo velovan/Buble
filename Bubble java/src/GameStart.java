19b1422bc0bfd1084e9301976102a5748299f77c
velovan <velovan@users.noreply.github.com>
2021-10-27 10:47:09 +0300

    Update GameStart.java

--- Bubble java/src/GameStart.java
+++ Bubble java/src/GameStart.java
@@ -5,15 +5,13 @@ public class GameStart {
 	public static void main(String [] args){
 		
 		GamePanel panel = new GamePanel();
-		
 		JFrame frame = new JFrame("Buble");
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setContentPane(panel);
-	    frame.pack();
+	        frame.pack();
 	 
-	    
 	    frame.setLocationRelativeTo(null);
 	    panel.start();
 	    frame.setVisible(true);
